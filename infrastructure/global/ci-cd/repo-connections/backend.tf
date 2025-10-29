a7a07aa5fac3de9b5da88b896cdb6f7c8050f269
Ian Paul Plasabas <iann.plasabas@gmail.com>
2025-10-30 07:19:38 +0800

    Fix Typos

--- infrastructure/global/ci-cd/repo-connections/backend.tf
+++ infrastructure/global/ci-cd/repo-connections/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "terraform-backend-89972986"
-    key          = "base-network/terraform.tfstate"
+    key          = "ci-cd/repo-connection/terraform.tfstate"
     region       = "ap-southeast-1"
     profile      = "plasabas-general-admin"
     use_lockfile = true
--- infrastructure/global/ci-cd/repo-connections/provider.tf
+++ infrastructure/global/ci-cd/repo-connections/provider.tf
@@ -2,14 +2,14 @@ terraform {
   required_version = "1.13.4"
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "6.18.0"
     }
   }
 }
 
 provider "aws" {
-  region = "ap-southeast-1"
+  region  = "ap-southeast-1"
   profile = "plasabas-general-admin"
   default_tags {
     tags = {
