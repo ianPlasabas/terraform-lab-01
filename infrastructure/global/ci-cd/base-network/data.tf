d6cf69a13da15f1086477ec9a80ae8e1da521577
Ian Paul Plasabas <iann.plasabas@gmail.com>
2025-10-30 07:26:44 +0800

    Added data.tf for tf reference for repo connection

---
+++ infrastructure/global/ci-cd/base-network/data.tf
@@ -0,0 +1,10 @@
+data "terraform_remote_state" "repo_connection" {
+  backend = "s3"
+  config = {
+    bucket       = "terraform-backend-89972986"
+    key          = "ci-cd/repo-connection/terraform.tfstate"
+    region       = "ap-southeast-1"
+    profile      = "plasabas-general-admin"
+    use_lockfile = true  
+  }
+}
